d736b5b21b597d9a5c9112e4dc8c04fe09359bdf
FravonDev <flvsadr@gmail.com>
2023-09-26 10:52:52 -0300

    feat: home components

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/flavio_adrianoo/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://github.com/FravonDev"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/flavioadriano/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
