3ba70c88560fac8dee014e837f2b7191c866395a
FravonDev <flvsadr@gmail.com>
2023-09-27 14:17:14 -0300

    feat: ScrollUp

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Skills from "./components/Skills/Skills";
+import ScrollUp from "./components/Scrollup/ScrollUp";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <About />
         <Skills />
       </main>
+      <ScrollUp />
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -60,15 +60,15 @@ const Header = () => {
 
             <li className="nav__item">
               <a
-                href="#portfolio"
-                onClick={() => setActiveNav("#portfolio")}
+                href="#projects"
+                onClick={() => setActiveNav("#projects")}
                 className={
-                  activeNav === "#portfolio"
+                  activeNav === "#projects"
                     ? "nav__link active-link"
                     : "nav__link"
                 }
               >
-                <i className="uil uil-scenery nav__icon"></i> Portfolio
+                <i className="uil uil-scenery nav__icon"></i> Projects
               </a>
             </li>
             <li className="nav__item">
@@ -81,7 +81,8 @@ const Header = () => {
                     : "nav__link"
                 }
               >
-                <i className="uil uil-file-alt nav__icon"></i> Qualification
+                <i className="uil uil-briefcase-alt nav__icon"></i>
+                Qualification
               </a>
             </li>
             <li className="nav__item">
